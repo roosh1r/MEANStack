09eb34724b0719de22862b59ddd5a04c897906dd
Ruchir Doshi <rd176h@att.com>
2016-05-12 16:03:44 -0500

    HW 1 made some progress

--- interface.js
+++ interface.js
@@ -3,6 +3,12 @@
  */
 exports.insert = function(db, doc, callback) {
   // TODO: implement
+  db.collection('movies').insert(doc, function(error, result) {
+    if(error){
+      console.log(error);
+      process.exit(1);
+    }
+  });
   callback(null);
 };
 
@@ -13,6 +19,17 @@ exports.insert = function(db, doc, callback) {
  *  http://mongodb.github.io/node-mongodb-native/2.0/api/Cursor.html#sort
  */
 exports.byDirector = function(db, director, callback) {
-  // TODO: implement
-  callback(null, []);
+  // TODO: implement]
+  var query = { "director" : director };
+  var sorter = { "title" : 1 };
+  var data;
+  db.collection('movies').find(query).nextObject( function (error, docs) {
+    if (error) {
+      console.log(error);
+      process.exit(1);
+    }
+    data = JSON.stringify(docs);
+    console.log(data);
+  });
+  callback(null, data);
 };
